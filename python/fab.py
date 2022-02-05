b53b0a3604ce6d563583319ae2204c7e4581273c
Fab <fabianjarrett@gmail.com>
2022-02-05 11:58:40 +0000

    Only import needed ctypes

--- python/fab.py
+++ python/fab.py
@@ -1,4 +1,4 @@
-import ctypes
+from ctypes import CDLL, c_char_p, c_float, c_int
 from sys import platform
 
 
@@ -10,22 +10,22 @@ elif platform.startswith('linux') or platform == 'linux':
     lib_name = "libfab.so"
 
 
-_libfab = ctypes.CDLL("../fab/build/" + lib_name)
+_libfab = CDLL("../fab/build/" + lib_name)
 
 
 def say_hello(name):
     return _libfab.say_hello(name.encode())
-_libfab.say_hello.argtypes = [ctypes.c_char_p]
+_libfab.say_hello.argtypes = [c_char_p]
 _libfab.say_hello.restype = None
 
 
 def add(a, b):
     return _libfab.add(a, b)
-_libfab.add.argtypes = [ctypes.c_int, ctypes.c_int]
-_libfab.add.restype = ctypes.c_int
+_libfab.add.argtypes = [c_int, c_int]
+_libfab.add.restype = c_int
 
 
 def approach(t, target, delta):
     return _libfab.approach(t, target, delta)
-_libfab.approach.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_float]
-_libfab.approach.restype = ctypes.c_float
+_libfab.approach.argtypes = [c_float, c_float, c_float]
+_libfab.approach.restype = c_float
