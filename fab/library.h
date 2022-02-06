a684f10a7e0ce035c44ee4b34df2b477dac58b5a
Fab <fabianjarrett@gmail.com>
2022-02-06 20:15:37 +0000

    Specify DLL needs exporting

--- fab/library.h
+++ fab/library.h
@@ -1,8 +1,17 @@
 #ifndef FAB_LIBRARY_H
 #define FAB_LIBRARY_H
 
-void say_hello(char* name);
-int add(int a, int b);
-float approach(float t, float target, float delta);
+// Windows needs to specify the DLL can be exported.
+#if defined(_WIN32)
+    #define FAB_EXPORT __declspec(dllexport)
+#endif
+
+#ifndef FAB_EXPORT
+    #define FAB_EXPORT
+#endif
+
+FAB_EXPORT void say_hello(char* name);
+FAB_EXPORT int add(int a, int b);
+FAB_EXPORT float approach(float t, float target, float delta);
 
 #endif //FAB_LIBRARY_H
