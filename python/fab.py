feb94b08e2ed0ee3a47e629e1c9387db137870f1
Fab <fabianjarrett@gmail.com>
2022-02-06 19:20:05 +0000

    Fix errors for windows

--- python/fab.py
+++ python/fab.py
@@ -4,14 +4,13 @@ from sys import platform
 
 
 if platform == "win32":
-    lib_name = "libfab.dll"
+    lib_name = "fab.dll"
 elif platform == "darwin":
     lib_name = "libfab.dylib"
 elif platform.startswith("linux") or platform == "linux":
     lib_name = "libfab.so"
 
-_libfab = CDLL(Path(__file__).parent.parent.joinpath("fab", "build", lib_name))
-
+_libfab = CDLL(str(Path(__file__).parent.parent.joinpath("fab", "build", lib_name)))
 
 def say_hello(name):
     return _libfab.say_hello(name.encode())
